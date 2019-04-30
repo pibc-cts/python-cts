6ceabd2b82ce2600ab07a448017f0d5128b231ea
citshares <citshares@gmail.com>
2019-04-30 01:58:37 -0700

    Modify the struct of functions

---
+++ market.py
@@ -0,0 +1,15 @@
+from util import *
+
+def get_order_book(base, quote, depth, port):
+        return post("get_order_book", [base,quote,depth], port)['result']
+
+
+def get_cny_settlement_price(port):
+        result = get_object(["2.4.0"], port)["result"]
+        cny = result[0]["current_feed"]['settlement_price']['base']['amount']
+        bts = result[0]["current_feed"]['settlement_price']['quote']['amount']
+        return float(cny) * 10 / float(bts)
+
+
+def publish_cny_feed_price(account, cts_amount, cny_amount, port):
+	return publish_feed_price(account, cts_amount, 'CNY', '1.3.1',cny_amount, port)
